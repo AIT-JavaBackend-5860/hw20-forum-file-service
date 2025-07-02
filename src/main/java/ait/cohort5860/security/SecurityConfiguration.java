16723181a76597c59ab9d8d14eae41d7499e6da3
Romyleads <romyleads@gmail.com>
2025-07-02 11:49:21 +0200

    lesson 19: add all auth methods and bugfix with BD

--- src/main/java/ait/cohort5860/security/SecurityConfiguration.java
+++ src/main/java/ait/cohort5860/security/SecurityConfiguration.java
@@ -1,9 +1,11 @@
 package ait.cohort5860.security;
 
 import ait.cohort5860.accounting.model.Role;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,7 +16,10 @@ import org.springframework.security.web.access.expression.WebExpressionAuthoriza
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final CustomWebSecurity webSecurity;
 
     @Bean
     SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -27,32 +32,65 @@ public class SecurityConfiguration {
 
                 authorize -> authorize
                         // .anyRequest().permitAll() - все разрешить(к примеру)
-                .requestMatchers("/account/register", "/forum/posts/**") // на регистрацию
-                    .permitAll() // пустить всех
+                        .requestMatchers("/account/register", "/forum/posts/**") // на регистрацию
+                        .permitAll() // пустить всех
 
-                .requestMatchers("/account/user/{login}/role/{role}") // добавлять роли
-                    .hasRole(Role.ADMINISTRATOR.name()) // должен юзер должен иметь роль админа
+                        .requestMatchers("/account/user/{login}/role/{role}") // добавлять роли
+                        .hasRole(Role.ADMINISTRATOR.name()) // должен юзер должен иметь роль админа
 
-                .requestMatchers(HttpMethod.PATCH, "/account/user/{login}","/forum/post/{id}/comment/{login}") // апдейтить можно только свой профиль
-                    .access(new WebExpressionAuthorizationManager("#login == authentication.name"))
+                        .requestMatchers(HttpMethod.PATCH, "/account/user/{login}", "/forum/post/{id}/comment/{login}") // апдейтить можно только свой профиль
+                        .access(new WebExpressionAuthorizationManager("#login == authentication.name"))
 
-                .requestMatchers(HttpMethod.POST, "/forum/post/{author}") // только автор от своего имени может добавлять пост
-                    .access(new WebExpressionAuthorizationManager("#author == authentication.name"))
+                        .requestMatchers(HttpMethod.POST, "/forum/post/{author}") // только автор от своего имени может добавлять пост
+                        .access(new WebExpressionAuthorizationManager("#author == authentication.name"))
 
-                //.requestMatchers(HttpMethod.PATCH, "/forum/post/{id}/comment/{author}") // только автор от своего имени может создавать/изменять комментарии
-                    //.access(new WebExpressionAuthorizationManager("#author == authentication.name"))
+                        //.requestMatchers(HttpMethod.PATCH, "/forum/post/{id}/comment/{author}") // только автор от своего имени может создавать/изменять комментарии
+                        //.access(new WebExpressionAuthorizationManager("#author == authentication.name"))
 
-                //.requestMatchers(HttpMethod.PATCH, "/forum/post/{postId}/comment/{commenter}") // только под своим именем комментатор может оставить комментарий
-                    //.access(new WebExpressionAuthorizationManager("#commenter == authentication.name"))
+                        //.requestMatchers(HttpMethod.PATCH, "/forum/post/{postId}/comment/{commenter}") // только под своим именем комментатор может оставить комментарий
+                        //.access(new WebExpressionAuthorizationManager("#commenter == authentication.name"))
 
-                .requestMatchers(HttpMethod.DELETE, "/account/user/{login}")
-                    .access(new WebExpressionAuthorizationManager("#login == authentication.name or hasRole('ADMINISTRATOR')"))
+                        .requestMatchers(HttpMethod.DELETE, "/account/user/{login}")
+                        .access(new WebExpressionAuthorizationManager("#login == authentication.name or hasRole('ADMINISTRATOR')"))
 
 
-                .anyRequest() // все
-                .authenticated()); // проверяется
 
-        return http.build();
+
+                        .requestMatchers(HttpMethod.PATCH, "/forum/post/{id}")
+                        .access(((authentication, context) ->
+                                new AuthorizationDecision(webSecurity.checkPostAuthor(context.getVariables().get("id"),
+                                        authentication.get().getName()))))
+
+                        .requestMatchers(HttpMethod.DELETE, "/forum/post/{id}")
+                        .access((authentication, context) -> {
+                            boolean isAuthor = webSecurity.checkPostAuthor(context.getVariables().get("id"),
+                                    authentication.get().getName());
+                            boolean isModerator = context.getRequest().isUserInRole(Role.MODERATOR.name());
+                            return new AuthorizationDecision(isAuthor || isModerator);
+                        })
+
+                /*
+
+                код учителя
+
+                .requestMatchers(HttpMethod.PATCH, "/forum/post/{id}")
+                .access(((authentication, context) ->
+                new AuthorizationDecision(webSecurity.checkPostAuthor(context.getVariables().get("id"),
+                authentication.get().getName()))))
+
+                .requestMatchers(HttpMethod.DELETE, "/forum/post/{id}")
+                .access((authentication, context) -> {
+                boolean isAuthor = webSecurity.checkPostAuthor(context.getVariables().get("id"),
+                authentication.get().getName());
+                boolean isModerator = context.getRequest().isUserInRole(Role.MODERATOR.name());
+                return new AuthorizationDecision(isAuthor || isModerator);
+                })
+
+                */
+                        .anyRequest() // все
+                        .authenticated()); // проверяется
+
+            return http.build();
     }
 
 
